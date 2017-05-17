9dde20fab97fe6917c06b19791551d4da351b333
matt.talamini <talamini714@gmail.com>
2017-05-17 12:48:54 -0400

    Added rhyming synonyms functionality to poetshelper.html

--- language/phScript.js
+++ language/phScript.js
@@ -1,10 +1,31 @@
 function PHCtrl($scope, $http) {
+//for debugging only
+window.MY_SCOPE = $scope;
 
 	var nodes = [];
 	var links = [];
 	
 	$scope.inputWord = 'mandate';
 	$scope.suggestions = [];
+
+	$scope.advanced = false;
+	$scope.input = {};
+	$scope.input.blueWord = 'mandate';
+	$scope.input.redWord = 'deep';
+
+	$scope.status = {};
+	$scope.status.redSynonymsCount = 0;
+	$scope.status.blueSynonymsCount = 0;
+	$scope.status.showSynonymsCount = false;
+	$scope.status.message = "";
+
+	$scope.bigRedList = [];
+	$scope.bigBlueList = [];
+	$scope.bigRedListIsComplete = false;
+	$scope.bigBlueListIsComplete = false;
+
+	$scope.redSuggestions = [];
+	$scope.blueSuggestions = [];
 	
 	function getRhymes(word, callback) {
 
@@ -106,7 +127,120 @@ function PHCtrl($scope, $http) {
 	var svg = d3.select("div.forcegraph").append("svg")
 	.attr("height", "100%")
 	.attr("width", "100%");
-	
+
+  $scope.easyButtonClick = function() {
+
+  	$scope.redSuggestions = [];
+  	$scope.blueSuggestions = [];
+  	$scope.status.blueSynonymsCount = 0;
+  	$scope.status.blueSynonymsTotal = 0;
+  	$scope.status.redSynonymsCount = 0;
+  	$scope.status.redSynonymsTotal = 0;
+  	$scope.status.showSynonymsCount = true;
+  	$scope.status.message = "";
+  	$scope.bigBlueList.length = 0;
+  	$scope.bigRedList.length = 0;
+
+    getSynonyms($scope.input.blueWord, function(synonyms) {
+        console.log("just got " + synonyms.length + " synonyms to " + $scope.input.blueWord);
+        $scope.status.blueSynonymsTotal = synonyms.length;
+        if (synonyms.length == 0) {
+            $scope.status.message = "I couldn't find " + $scope.input.blueWord + " in my thesaurus";
+        }
+        synonyms.map(function(v, i){
+
+            getRhymes(v, function(rhymes){
+                $scope.status.blueSynonymsCount++;
+                $scope.bigBlueList.push({word: v, rhymes: rhymes});
+                if ($scope.bigBlueList.length == synonyms.length) {
+                    $scope.bigBlueListIsComplete = true;
+                    if ($scope.bigRedListIsComplete) {
+                        $scope.bigBlueListIsComplete = false;
+                        $scope.bigRedListIsComplete = false;
+                        bothListsComplete();
+                    }
+                }
+            });
+        });
+    });
+
+    getSynonyms($scope.input.redWord, function(synonyms) {
+        console.log("just got " + synonyms.length + " synonyms to " + $scope.input.redWord);
+        $scope.status.redSynonymsTotal = synonyms.length;
+        if (synonyms.length == 0) {
+            $scope.status.message = "I couldn't find " + $scope.input.redWord + " in my thesaurus";
+        }
+        synonyms.map(function(v, i){
+
+            getRhymes(v, function(rhymes){
+                $scope.status.redSynonymsCount++;
+                $scope.bigRedList.push({word: v, rhymes: rhymes});
+                    if ($scope.bigRedList.length == synonyms.length) {
+                        $scope.bigRedListIsComplete = true;
+                        if ($scope.bigBlueListIsComplete) {
+                            $scope.bigBlueListIsComplete = false;
+                            $scope.bigRedListIsComplete = false;
+                            bothListsComplete();
+                        }
+                    }
+            });
+        });
+    });
+
+  }
+
+  function bothListsComplete() {
+console.info("starting bothlistscomplete");
+    var flatRedRhymes = [];
+    //var flatBlueRhymes = [];
+    //var flatRedSynonyms = [];
+    var flatBlueSynonyms = [];
+
+    for (i in $scope.bigRedList) {
+        flatRedRhymes = flatRedRhymes.concat(
+            $scope.bigRedList[i].rhymes.map(function(v){
+                return {word: v, cameFrom: $scope.bigRedList[i].word}
+            })
+
+        );
+        //flatRedSynonyms.push($scope.bigRedList[i].word);
+    }
+
+    for (i in $scope.bigBlueList) {
+        /*flatBlueRhymes = flatBlueRhymes.concat(
+
+            $scope.bigBlueList[i].rhymes.map(function(v){
+                return {word: v, cameFrom: $scope.bigBlueList[i].word}
+            })
+        );*/
+        flatBlueSynonyms.push($scope.bigBlueList[i].word);
+    }
+
+    console.info(flatBlueSynonyms.length + "    " + flatRedRhymes.length);
+
+    //we want to find red rhymes equal to blue synonyms & vice versa
+
+    /*$scope.redSuggestions = flatBlueRhymes.filter(function(n) {
+        return flatRedSynonyms.indexOf(n.word) !== -1;
+    });*/
+
+
+    var temp = flatRedRhymes.filter(function(n) {
+        return flatBlueSynonyms.indexOf(n.word) !== -1;
+    });
+
+    $scope.blueSuggestions = temp.filter(function(v) {
+        return (v.word !== v.cameFrom);
+    })
+
+    if ($scope.blueSuggestions.length > 0) {
+      $scope.status.message = "Here are " + $scope.blueSuggestions.length + " rhyming synonyms:";
+    } else {
+      $scope.status.message = "Sorry, couldn't find any intersections.";
+    }
+
+  }
+
   $scope.buttonClick = function() {
     console.info($scope.inputWord);
 
